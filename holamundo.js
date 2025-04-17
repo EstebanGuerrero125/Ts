9e1afe29cbcf361825f29918a2643680bf3f1250
warqlq <davidguerrerinho@gmail.com>
2025-04-17 01:16:14 -0500

    first commit

---
+++ holamundo.js
@@ -0,0 +1,10 @@
+"use strict";
+const ventas = {
+    total: 50,
+    cantidad: 2,
+    precio: 20,
+    nombre: "ropa"
+};
+ventas.total = 70;
+console.log(ventas.total + 10);
+console.log("si concatena: " + ventas.total + 10);
---
+++ holamundo.ts
@@ -0,0 +1,11 @@
+const ventas ={
+    total:50,
+    cantidad:2,
+    precio:20,
+    nombre:"ropa"
+}
+ventas.total=70
+
+console.log(ventas.total+10);
+
+console.log("si concatena: "+ventas.total+10);
