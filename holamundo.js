34a822f8054bd1f7297f0cbdbe0dd2e428f74750
warqlq <davidguerrerinho@gmail.com>
2025-04-18 01:41:28 -0500

    probando botones

--- holamundo.js
+++ holamundo.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a, _b;
 const ventas = {
     total: 50,
     cantidad: 2,
@@ -10,6 +11,16 @@ const pedido = {
     edad: 45
 };
 ventas.total = 70;
+const saludo = document.getElementById('saludo');
+const texto = document.getElementById('texto');
+if (saludo) {
+    saludo.textContent = 'probando desde saludo';
+}
+if (texto) {
+    texto.textContent = 'probando desde texto';
+}
+(_a = document.getElementById('boton1')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => alert("Probando desde saludo"));
+(_b = document.getElementById('boton2')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => alert("Probando desde texto"));
 console.log(ventas.total + 10);
 console.log("si concatena: " + ventas.total + 10);
 console.log("Prueba: " + pedido.nombre);
--- holamundo.ts
+++ holamundo.ts
@@ -10,7 +10,20 @@ const pedido ={
     edad:45
 }
 ventas.total=70
+const saludo = document.getElementById('saludo');
 
+const texto = document.getElementById('texto');
+
+if (saludo) {
+    saludo.textContent = 'probando desde saludo';
+  }
+
+if(texto){
+    texto.textContent = 'probando desde texto';
+}
+
+document.getElementById('boton1')?.addEventListener('click',()=>alert("Probando desde saludo"))
+document.getElementById('boton2')?.addEventListener('click',()=>alert("Probando desde texto"))
 console.log(ventas.total+10);
 
 console.log("si concatena: "+ventas.total+10);
